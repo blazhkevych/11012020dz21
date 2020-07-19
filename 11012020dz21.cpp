2ee1b6761c2f61ae0c47b890fd99d2a2fb7d8db7
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:32:20 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz21.cpp
@@ -0,0 +1,17 @@
+﻿/*
+** Пользователь 	вводит с клавиатуры двухзначное число.Вывести на экран количество сотен и 	десятков этого числа.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите с клавиатуры двузначное число: ";
+	int a, b, c;
+	cin >> a;
+	b = a / 10;
+	c = a / 100;
+	cout << int(c) << " сотен, " << int(b) << " десятков." << endl;
+	return 0;
+}
